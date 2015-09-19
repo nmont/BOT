7dbeec44a9674d6d45d1bb17893a187416ee63da
Ben Williams <bww1@iastate.edu>
2015-09-19 00:21:19 -0500

    Added in instructions parsing logic

--- api/api.py
+++ api/api.py
@@ -2,6 +2,7 @@ __author__ = 'Ben Williams'
 
 import Instruction
 import InstructionList
+import json
 
 LEFT_BUMPER_START = 10
 LEFT_BUMPER_END = 11
@@ -11,16 +12,27 @@ RIGHT_BUMPER_END = 13
 
 # Takes in string file name
 # outputs fulled constructed instruction list
-def txt_to_instruction_list(file_name):
+def json_to_instruction_list(file_name):
     f = open(file_name, 'r')
-    i = InstructionList()
-    for line in f:
-        instruction = parse_instruction(line)
-        i.append_instruction(instruction)
-    return i
+    instructions = InstructionList()
+    json_list = json.load(f)
+    for item in json_list:
+        instruction = parse_instruction(item)
+        instructions.append_instruction(instruction)
+    return instructions
 
 
 # TODO - Implement lookup for instruction numbers
 def parse_instruction(string):
-    i = Instruction(int(string))
+    instruction = Instruction(int(string))
+    return instruction
+
+def instruction_list_to_json(instructions, file_name):
+    if type(instructions) != InstructionList:
+        return -1
+    f = open(file_name, 'w')
+    f.write(json.dump(instructions))
+    for item in json_list:
+        instruction = parse_instruction(item)
+        i.append_instruction(instruction)
     return i
