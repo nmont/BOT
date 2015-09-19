23d06ba8b5e327fd3bad0503f64d1ac900d77cc7
Ben Williams <bww1@iastate.edu>
2015-09-19 11:21:40 -0500

    Updated json load funciton

--- api/test.py
+++ api/test.py
@@ -15,5 +15,5 @@ instructions.append_instruction(api.DONE)
 instructions.append_instruction(api.RIGHT_BUMPER_END)
 instructions.append_instruction(api.GOTO_START)
 
-api.instruction_list_to_json(instructions)
-# api.json_to_instruction_list(json_string)
+json_string = api.instruction_list_to_json(instructions)
+api.json_to_instruction_list(json_string)
